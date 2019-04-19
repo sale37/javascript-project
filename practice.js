236b87708d5998f708a34f4433d9d7e48901e10c
sale37 <sale-37@live.com>
2019-04-19 14:27:02 +0200

    promises

--- practice.js
+++ practice.js
@@ -107,3 +107,28 @@
 // for(i = 0; i < arrayJs.length; i++){
 //     console.log(arrayJs[i]);
 // }
+
+// PROMISES =======================================================
+
+let promise = new Promise(function(resolve, reject){
+    setTimeout(() => resolve('Success!'), 2000);
+});
+
+let promise2 = new Promise(function(resolve, reject){
+    setTimeout(() => reject(new Error('Error')), 2000);
+});
+
+let promise3 = new Promise(function(resolve, reject){
+    setTimeout(() => resolve('Success!'), 2000);
+});
+
+let promise4 = Promise.all([promise, promise3]);
+
+promise.then(console.log);
+promise2.catch(error => console.log(error.message));
+promise3.then(console.log);
+
+
+promise4.then(
+    data => console.log(data),
+    error => console.log(error));
