67e91b2e09360308b652429836cd9176901f1ce3
sale37 <sale-37@live.com>
2019-04-19 10:16:49 +0200

    functions

--- practice.js
+++ practice.js
@@ -3,7 +3,26 @@ function myFunction(){
         var num = i;
         console.log(num);
     } 
-}
+};
+
+var numbers = [1, 16, 25];
+
+var square = function(){
+    var numbers2 = numbers.map(Math.sqrt);
+    return numbers2;
+};
+
+(function() {
+    console.log(2+2);
+  })();
+
+  var timesTwo = (number) => {return number * 2};
+    
 
 
-myFunction()
+
+// myFunction()
+
+// console.log(square());
+
+console.log(timesTwo(5));
