dc5006b9b15d08cbb86a84f31ad0db7a67c6789e
sale37 <sale-37@live.com>
2019-04-19 13:36:40 +0200

    arrays

--- practice.js
+++ practice.js
@@ -1,28 +1,88 @@
-function myFunction(){
-    for(i = 0; i < 3; i++){
-        var num = i;
-        console.log(num);
-    } 
-};
+// SCOPE ===========================================
 
-var numbers = [1, 16, 25];
+// function myFunction(){
+//     for(i = 0; i < 3; i++){
+//         var num = i;
+//         console.log(num);
+//     } 
+// };
 
-var square = function(){
-    var numbers2 = numbers.map(Math.sqrt);
-    return numbers2;
-};
+// var numbers = [1, 16, 25];
 
-(function() {
-    console.log(2+2);
-  })();
+// var square = function(){
+//     var numbers2 = numbers.map(Math.sqrt);
+//     return numbers2;
+// };
 
-  var timesTwo = (number) => {return number * 2};
+// (function() {
+//     console.log(2+2);
+//   })();
+
+//   var timesTwo = (number) => {return number * 2};
     
-
-
-
 // myFunction()
 
 // console.log(square());
 
-console.log(timesTwo(5));
+// console.log(timesTwo(5));
+
+// ARRAYS =============================================
+
+// var colours = ['red', 'green', 'blue'];
+
+// function coloursFunction(){
+// for(var i = 0; i < colours.length; i++){
+//     console.log(colours[i]);
+// }
+// };
+
+// coloursFunction();
+
+// colours.forEach(function(element) {
+//     console.log(element);
+//   });
+
+// var reversedArray = colours.reverse();
+
+// console.log(reversedArray);
+
+// var arrayNumbers = [];
+
+// for(i = 1; i < 5; i++){
+//     arrayNumbers.push(i);
+// }
+
+// let total = arrayNumbers.reduce(function(sum, item){
+//     return sum + item;
+//   }, 50);
+
+//   console.log(total);
+
+// function arrayFunction(n){
+//     let newArray = [];
+//     for(i = 0; i <= n; i++){
+//         newArray.push(i);
+//     }
+//     for(i = n-1; i >= 0; i--){
+//         newArray.push(i);
+//     }
+//     return newArray;
+// }
+
+// console.log(arrayFunction(10));
+
+// var myZoo = [
+//     ["King Kong", ["gorilla", 42]],
+//     ["Nemo", ["fish", 5]],
+//     ["Punxsutawney Phil", ["groundhog", 11]]
+//   ];
+
+//   function zooInventory(array){
+//       for(i = 0; i < array.length; i++){
+//           var text = array[i][0] + ' the ' + array[i][1][0] + ' is ' + array[i][1][1];
+//           console.log(text);
+//       }
+//   }
+
+//   zooInventory(myZoo);
+
